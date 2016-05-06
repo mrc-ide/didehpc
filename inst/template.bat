33789e93a6ddda2bf8c91a77ddc33a5d4688a42a
Rich FitzJohn <rich.fitzjohn@gmail.com>
2016-05-06 12:17:03 +0100

    Support for error propagation when using recent context
    Fixes #3

--- inst/template.bat
+++ inst/template.bat
@@ -7,6 +7,7 @@ set CONTEXT_WORKDRIVE={{{context_workdrive}}}
 set CONTEXT_WORKDIR={{{context_workdir}}}
 set CONTEXT_ROOT={{{context_root}}}
 set CONTEXT_LOGFILE={{{context_logfile}}}
+set CONTEXT_PROPAGATE_ERROR=TRUE
 
 call setr{{{r_version}}}.bat
 
@@ -24,3 +25,4 @@ cd \%CONTEXT_WORKDIR%
 cd
 echo Logging to %CONTEXT_LOGFILE%
 Rscript %CONTEXT_ROOT%\bin\context_runner %CONTEXT_ROOT% %CONTEXT_TASK_ID% > %CONTEXT_LOGFILE% 2>&1
+echo Task completed successfully
