7f422bc5781717a3f9bcd79eccac9439e9ec7ceb
Rich FitzJohn <rich.fitzjohn@gmail.com>
2016-05-13 12:19:08 +0100

    Chattier logging on startup

--- inst/template.bat
+++ inst/template.bat
@@ -1,7 +1,9 @@
 REM automatically generated
+REM   host: %COMPUTERNAME%
 REM   date: {{{date}}}
 REM   didewin version: {{{didewin_version}}}
 REM   context version: {{{context_version}}}
+REM   dide task id: %CCP_TASKSYSTEMID%
 set CONTEXT_TASK_ID={{{context_task_id}}}
 set CONTEXT_WORKDRIVE={{{context_workdrive}}}
 set CONTEXT_WORKDIR={{{context_workdir}}}
@@ -12,6 +14,7 @@ set CONTEXT_PROPAGATE_ERROR=TRUE
 call setr{{{r_version}}}.bat
 
 {{{#network_shares}}}
+REM "mapping {{{drive}}} => {{{path}}}"
 net use {{{drive}}} {{{path}}} /y
 {{{/network_shares}}}
 
@@ -22,7 +25,8 @@ set CONTEXT_CORES=%CCP_NUMCPUS%
 
 %CONTEXT_WORKDRIVE%
 cd \%CONTEXT_WORKDIR%
-cd
-echo Logging to %CONTEXT_LOGFILE%
+REM   working directory: %CD%
+
+REM   logfile: %CONTEXT_LOGFILE%
 Rscript %CONTEXT_ROOT%\bin\context_runner %CONTEXT_ROOT% %CONTEXT_TASK_ID% > %CONTEXT_LOGFILE% 2>&1
 echo Task completed successfully
