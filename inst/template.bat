6889b4258a0967bcf09d25bc92d7ecc4f845b11e
Rich FitzJohn <rich.fitzjohn@gmail.com>
2016-05-16 11:32:35 +0100

    More tweaks to server log format

--- inst/template.bat
+++ inst/template.bat
@@ -1,10 +1,10 @@
 @echo off
 REM automatically generated
-ECHO   host: %COMPUTERNAME%
-ECHO   date: {{{date}}}
-ECHO   didewin version: {{{didewin_version}}}
-ECHO   context version: {{{context_version}}}
-ECHO   dide task id: %CCP_TASKSYSTEMID%
+ECHO host: %COMPUTERNAME%
+ECHO date: {{{date}}}
+ECHO didewin version: {{{didewin_version}}}
+ECHO context version: {{{context_version}}}
+ECHO dide task id: %CCP_TASKSYSTEMID%
 set CONTEXT_TASK_ID={{{context_task_id}}}
 set CONTEXT_WORKDRIVE={{{context_workdrive}}}
 set CONTEXT_WORKDIR={{{context_workdir}}}
@@ -15,19 +15,22 @@ set CONTEXT_PROPAGATE_ERROR=TRUE
 call setr{{{r_version}}}.bat
 
 {{{#network_shares}}}
-ECHO   mapping {{{drive}}} -^> {{{path}}}
+ECHO mapping {{{drive}}} -^> {{{path}}}
 net use {{{drive}}} {{{path}}} /y
 {{{/network_shares}}}
 
 {{{#parallel}}}
-ECHO   This is a parallel job: will use %CPP_NUMCPUS%
+ECHO This is a parallel job: will use %CPP_NUMCPUS%
 set CONTEXT_CORES=%CCP_NUMCPUS%
 {{{/parallel}}}
 
 %CONTEXT_WORKDRIVE%
 cd \%CONTEXT_WORKDIR%
-ECHO   working directory: %CD%
+ECHO working directory: %CD%
 
-ECHO   logfile: %CONTEXT_LOGFILE%
+ECHO logfile: %CONTEXT_LOGFILE%
+
+ECHO on
 Rscript %CONTEXT_ROOT%\bin\context_runner %CONTEXT_ROOT% %CONTEXT_TASK_ID% > %CONTEXT_LOGFILE% 2>&1
-echo Task completed successfully
+
+@echo Quitting
