44c2b9ba9cbafcc85c3c6afcc055f7d1eb91902b
Rich FitzJohn <rich.fitzjohn@gmail.com>
2017-02-20 17:11:29 +0000

    Test of complex compiled dependencies on linux

---
+++ tests/testthat/test-linux-deps.R
@@ -0,0 +1,30 @@
+context("linux (complex dependencies)")
+
+test_that("linux2", {
+  ## TODO: this is not going to work in the cases where a package has
+  ## complex dependencies; I should update buildr to respect Remotes:
+  ## fields in DESCRIPTION, which would be the easiest way of
+  ## supporting that I think (though discounting packages that are
+  ## already uploaded would be necessary).
+  skip_on_travis()
+  skip_if_not_installed("buildr")
+
+  owd <- prepare_didewin("linux2", "mysources.R")
+  on.exit(setwd(owd))
+
+  config <- didewin_config(cluster = "fi--didelxhn")
+  github <- c("jeffeaton/anclik/anclik",
+              "jeffeaton/epp",
+              "jeffeaton/eppspectrum@allcausemx",
+              "s-u/fastmatch")
+  src <- provisionr::package_sources(github = github)
+  root <- "context"
+  ctx <- context::context_save(root,
+                               packages = c("fastmatch", "eppspectrum"),
+                               package_sources = src)
+  obj <- didewin::queue_didewin(ctx, config = config)
+
+  t <- obj$enqueue(sessionInfo())
+  res <- t$wait(10, progress = FALSE)
+  res
+})
