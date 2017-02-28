4fb60416a8e5e15e478069ffb2e5998e0d5da796
Rich FitzJohn <rich.fitzjohn@gmail.com>
2017-02-28 12:25:14 +0000

    Fix configuration testing
    Was totally not updated from didewin

--- tests/testthat/test-0-config.R
+++ tests/testthat/test-0-config.R
@@ -1,45 +1,40 @@
 context("config")
 
-## Always a hassle.  I should write a package for package configurations...
-test_that("globals are not usually defined", {
-  expect_null(getOption("didehpc.credentials"))
-  expect_null(getOption("didehpc.home"))
-  expect_null(getOption("didehpc.temp"))
-  expect_null(getOption("didehpc.cluster"))
-})
-
 test_that("credentials", {
   if (!interactive()) {
     expect_error(get_credentials(NULL), "Credentials file needed")
     expect_error(get_credentials("username", TRUE), "Credentials file needed")
   }
   expect_equal(get_credentials("username", FALSE),
-               list(username="username"))
+               list(username = "username"))
   expect_equal(get_credentials("DIDE\\username", FALSE),
-               list(username="username"))
+               list(username = "username"))
 
   ## Lists:
-  expect_equal(get_credentials(list(username="username"), FALSE),
-               list(username="username"))
-  expect_error(get_credentials(list(username="username"), TRUE),
+  expect_equal(get_credentials(list(username = "username"), FALSE),
+               list(username = "username"))
+  expect_error(get_credentials(list(username = "username"), TRUE),
                "Missing fields")
-  expect_equal(get_credentials(list(username="username", password="pw"), FALSE),
-               list(username="username", password="pw"))
-  expect_equal(get_credentials(list(username="username", password="pw"), TRUE),
-               list(username="username", password="pw"))
+  expect_equal(
+    get_credentials(list(username = "username", password = "pw"), FALSE),
+    list(username = "username", password = "pw"))
+  expect_equal(
+    get_credentials(list(username = "username", password = "pw"), TRUE),
+    list(username = "username", password = "pw"))
 
-  expect_error(get_credentials(list(username="foo", password="bar", extra="x")),
+  expect_error(
+    get_credentials(list(username = "foo", password = "bar", extra = "x")),
                "Unknown fields")
   expect_error(get_credentials(list("username")),
                "must be named")
   expect_error(get_credentials(list()),
                "must be named")
-  expect_error(get_credentials(list(password="foo")),
+  expect_error(get_credentials(list(password = "foo")),
                "Missing fields")
 
-  writeLines(c("username=us", "password=pw"), tmp <- tempfile())
+  writeLines(c("username = us", "password = pw"), tmp <- tempfile())
   expect_equal(get_credentials(tmp, FALSE),
-               list(username="us", password="pw"))
+               list(username = "us", password = "pw"))
 })
 
 test_that("interactive", {
@@ -51,67 +46,60 @@ test_that("interactive", {
   expect_equal(dat$credentials, "me")
   expect_equal(dat$username, "me")
   expect_null(dat$password)
-  expect_equal(dat$cluster, valid_clusters()[[1]])
   ## This might become an error soon because there will be nothing to
   ## map against.
   expect_is(dat$shares, "list")
 
-  dat <- didehpc_config("me", home=tempdir())
+  dat <- didehpc_config("me", home = tempdir())
 
   expect_equal(dat$shares$home,
-               structure(list(name="home",
-                              path_remote=clean_path(dide_home("", "me")),
-                              path_local=tempdir(),
-                              drive_remote="Q:"),
-                         class="path_mapping"))
-})
-
-test_that("globals", {
-  oo <- didehpc_config_global(credentials="foo")
-  on.exit(options(oo))
-  expect_equal(oo, list(didehpc.credentials=NULL))
+               structure(list(name = "home",
+                              path_remote = clean_path(dide_home("", "me")),
+                              path_local = tempdir(),
+                              drive_remote = "Q:"),
+                         class = "path_mapping"))
 })
 
 test_that("unset globals", {
   oo <- options()
   on.exit(options(oo))
 
-  didehpc_config_global(credentials="foo")
+  didehpc_config_global(credentials = "foo")
   expect_equal(getOption("didehpc.credentials"), "foo")
 
-  didehpc_config_global(credentials=NULL)
+  didehpc_config_global(credentials = NULL)
   expect_equal(getOption("didehpc.credentials"), NULL)
   expect_equal(getOption("didehpc.credentials", NA), NA) # really not set
 })
 
 test_that("template logic", {
-  oo <- didehpc_config_global(credentials="foo")
+  oo <- didehpc_config_global(credentials = "foo", cluster = "fi--dideclusthn")
   on.exit(options(oo))
 
   expect_equal(didehpc_config()$resource,
-               list(parallel=FALSE, count=1, type="Cores"))
-  expect_equal(didehpc_config(wholenode=TRUE)$resource,
-               list(parallel=TRUE, count=1, type="Nodes"))
-  expect_equal(didehpc_config(wholenode=TRUE, parallel=TRUE)$resource,
-               list(parallel=TRUE, count=1, type="Nodes"))
-  expect_equal(didehpc_config(wholenode=TRUE, parallel=FALSE)$resource,
-               list(parallel=FALSE, count=1, type="Nodes"))
+               list(parallel = FALSE, count = 1, type = "Cores"))
+  expect_equal(didehpc_config(wholenode = TRUE)$resource,
+               list(parallel = TRUE, count = 1, type = "Nodes"))
+  expect_equal(didehpc_config(wholenode = TRUE, parallel = TRUE)$resource,
+               list(parallel = TRUE, count = 1, type = "Nodes"))
+  expect_equal(didehpc_config(wholenode = TRUE, parallel = FALSE)$resource,
+               list(parallel = FALSE, count = 1, type = "Nodes"))
 
-  expect_error(didehpc_config(wholenode=TRUE, cores=2)$resource,
+  expect_error(didehpc_config(wholenode = TRUE, cores = 2)$resource,
                "Cannot specify both wholenode and cores")
 
-  expect_equal(didehpc_config(template="8Core")$resource,
-               list(parallel=TRUE, count=1, type="Nodes"))
-  expect_equal(didehpc_config(template="8Core", parallel=FALSE)$resource,
-               list(parallel=FALSE, count=1, type="Nodes"))
-  expect_equal(didehpc_config(template="8Core", cores=3)$resource,
-               list(parallel=TRUE, count=3, type="Cores"))
+  expect_equal(didehpc_config(template = "8Core")$resource,
+               list(parallel = TRUE, count = 1, type = "Nodes"))
+  expect_equal(didehpc_config(template = "8Core", parallel = FALSE)$resource,
+               list(parallel = FALSE, count = 1, type = "Nodes"))
+  expect_equal(didehpc_config(template = "8Core", cores = 3)$resource,
+               list(parallel = TRUE, count = 3, type = "Cores"))
 })
 
 test_that("parallel and cores", {
-  oo <- didehpc_config_global(credentials="foo")
+  oo <- didehpc_config_global(credentials = "foo")
   on.exit(options(oo))
   expect_false(didehpc_config()$resource$parallel)
-  expect_false(didehpc_config(cores=1)$resource$parallel)
-  expect_true(didehpc_config(cores=2)$resource$parallel)
+  expect_false(didehpc_config(cores = 1)$resource$parallel)
+  expect_true(didehpc_config(cores = 2)$resource$parallel)
 })
