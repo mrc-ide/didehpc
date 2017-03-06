a3ed2dfd5ea3894f8fe9170d9dbebf6e0eb4daff
Rich FitzJohn <rich.fitzjohn@gmail.com>
2017-03-06 17:05:02 +0000

    Speed up and tweak tests

--- tests/testthat/test-3-sql.R
+++ tests/testthat/test-3-sql.R
@@ -16,15 +16,13 @@ test_that("sql", {
   obj <- didehpc::queue_didehpc(ctx, config = list(use_common_lib = TRUE))
 
   expect_is(obj, "queue_didehpc")
-  path_lib <- file.path(path, "lib", "windows", obj$config$r_version[1, 1:2])
-  expect_true(file.exists(path_lib))
-  expect_true(file.exists(file.path(path_lib, "context")))
 
   t <- obj$enqueue(sessionInfo())
   res <- t$wait(10, progress = FALSE)
 
-  ## This *should* have produced a progress bar
-  grp <- obj$lapply(1:100, quote(sin))
+  grp <- obj$lapply(1:4, quote(sin))
+  ans <- grp$wait(10)
+  expect_equal(ans, as.list(sin(1:4)))
 
   ## Cleanup
   obj$db$destroy()
