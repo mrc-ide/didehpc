32fb834525a1dd24613cf477c905faeadb087568
Rich FitzJohn <rich.fitzjohn@gmail.com>
2017-04-25 10:59:02 +0100

    Test reprovisioning a binary package

--- tests/testthat/test-4-compiled-dep.R
+++ tests/testthat/test-4-compiled-dep.R
@@ -55,3 +55,46 @@ test_that("manual compilation", {
   msg <- capture_messages(obj <- didehpc::queue_didehpc(ctx))
   expect_false(any(grepl("drat", msg)))
 })
+
+test_that("update binary", {
+  ## This is a _giant_ faff because I need to pull down and _extract_
+  ## seagull.
+  skip_on_travis()
+  skip_if_not_installed("buildr")
+
+  owd <- prepare_didehpc("compiled")
+  on.exit(setwd(owd))
+
+  src <- provisionr::package_sources(github = "richfitz/seagull")
+  tmp <- src$clone()$build()
+  file <- dir(file.path(tmp$local_drat, "src", "contrib"),
+              pattern = "^seagull_.*\\.tar\\.gz$", full.names = TRUE)
+  provisionr:::drat_storr(tmp$local_drat)$get("github::richfitz/seagull")
+  file <- dir(file.path(tmp$local_drat, "src", "contrib"),
+              pattern = "^seagull_.*\\.tar\\.gz$", full.names = TRUE)
+  untar(file, exdir = ".")
+  seagull <- "seagull"
+
+  v0 <- read_version(seagull)
+
+  path <- "context"
+  src <- provisionr::package_sources(local = seagull)
+  ctx <- context::context_save(path,
+                               packages = "seagull",
+                               package_sources = src)
+  obj <- didehpc::queue_didehpc(ctx, config = list(use_common_lib = TRUE))
+
+  path_lib_win <- context:::path_library(path, "windows", obj$config$r_version)
+  path_drat <- file.path(path, "drat")
+
+  vi <- read_version(file.path(path_lib_win, "seagull"))
+  expect_identical(vi, v0)
+
+  ## Then update things:
+  v1 <- alter_package_version(seagull, TRUE)
+  obj$provision()
+  expect_identical(read_version(file.path(path_lib_win, "seagull")), v0)
+
+  obj$provision(refresh_drat = TRUE)
+  expect_identical(read_version(file.path(path_lib_win, "seagull")), v1)
+})
